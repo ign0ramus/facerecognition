cc3d511636e2b034c75f63ed59314351bf80b6a5
ign0ramus <yanushkevich_dmitry@mail.ru>
2019-12-20 13:35:42 +0300

    add face recognition feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 import Rank from './components/Rank/Rank';
+import Facerecognition from './components/Facerecognition/Facerecognition';
+
+const clarifaiApp = new Clarifai.App({
+	apiKey: 'test',
+});
 
 const particlesOptions = {
 	particles: {
@@ -13,21 +19,57 @@ const particlesOptions = {
 			density: {
 				enable: true,
 				value_area: 800,
-			}
-		}
+			},
+		},
 	},
 };
 
 function App() {
+	const [inputUrl, setInputUrl] = useState('');
+	const [boundingBoxes, setBoundingBoxes] = useState(null);
+
+	const handleChange = e => {
+		setBoundingBoxes(null);
+		setInputUrl(e.target.value);
+	};
+
+	const onClick = async () => {
+		try {
+			const res = await clarifaiApp.models.predict(
+				Clarifai.FACE_DETECT_MODEL,
+				inputUrl
+			);
+			setBoundingBoxesPosition(res);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	const setBoundingBoxesPosition = data => {
+		const detectedFaces = data.outputs[ 0 ].data.regions;
+		if (detectedFaces) {
+			const facesCooords = detectedFaces.map(el => {
+				const coords = el.region_info.bounding_box;
+				return {
+					id: el.id,
+					top: coords.top_row * 100,
+					left: coords.left_col * 100,
+					right: (1 - coords.right_col) * 100,
+					bottom: (1 - coords.bottom_row) * 100,
+				};
+			});
+			setBoundingBoxes(facesCooords);
+		}
+	};
+
 	return (
 		<div className='App'>
 			<Particles className='particles' params={particlesOptions} />
 			<Navigation />
 			<Logo />
 			<Rank />
-			<ImageLinkForm />
-			{/*
-        <FaceRecognition/> */}
+			<ImageLinkForm onChange={handleChange} onClick={onClick} />
+			<Facerecognition url={inputUrl} boxes={boundingBoxes} />
 		</div>
 	);
 }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => (
+const ImageLinkForm = ({ onChange, onClick }) => (
 	<div>
 		<p className='f3'>This Magic Brain will detect faces in your pictures.</p>
 		<div className='flex justify-center'>
 			<div className='form center pa4 br3 shadow-5 flex items-center'>
-				<input className='f4 pa2 w-70 center' type='text' />
-				<button className='w-30 flex justify-center grow f4 h-100 br2 link pv2 dib white bg-light-purple ba b--purple'>
+				<input onChange={onChange} className='f4 pa2 w-70 center' type='text' />
+				<button
+					onClick={onClick}
+					className='w-30 flex justify-center grow f4 h-100 br2 link pv2 dib white bg-light-purple ba b--purple'
+				>
 					Detect
 				</button>
 			</div>
