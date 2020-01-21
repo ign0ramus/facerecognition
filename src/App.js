8570ace40b1a92ab300dab66eba87ca1801d7fb3
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-21 12:40:20 +0300

    add signIn and singUp forms

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 import Rank from './components/Rank/Rank';
 import Facerecognition from './components/Facerecognition/Facerecognition';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 
 const clarifaiApp = new Clarifai.App({
 	apiKey: 'test',
@@ -24,16 +26,17 @@ const particlesOptions = {
 	},
 };
 
-function App() {
+const App = () => {
 	const [inputUrl, setInputUrl] = useState('');
 	const [boundingBoxes, setBoundingBoxes] = useState(null);
+	const [route, setRoute] = useState('signIn');
 
 	const handleChange = e => {
 		setBoundingBoxes(null);
 		setInputUrl(e.target.value);
 	};
 
-	const onClick = async () => {
+	const handleImageLinkFormClick = async () => {
 		try {
 			const res = await clarifaiApp.models.predict(
 				Clarifai.FACE_DETECT_MODEL,
@@ -42,36 +45,52 @@ function App() {
 			setBoundingBoxesPosition(res);
 		} catch (err) {
 			console.error(err);
+			alert('Something went wrong! :(');
 		}
 	};
 
 	const setBoundingBoxesPosition = data => {
-		const detectedFaces = data.outputs[ 0 ].data.regions;
+		const detectedFaces = data.outputs[0].data.regions;
 		if (detectedFaces) {
-			const facesCooords = detectedFaces.map(el => {
-				const coords = el.region_info.bounding_box;
-				return {
-					id: el.id,
-					top: coords.top_row * 100,
-					left: coords.left_col * 100,
-					right: (1 - coords.right_col) * 100,
-					bottom: (1 - coords.bottom_row) * 100,
-				};
-			});
-			setBoundingBoxes(facesCooords);
+			const facesCoords = calculateFacesCoords(detectedFaces);
+			setBoundingBoxes(facesCoords);
 		}
 	};
+
+	const calculateFacesCoords = detectedFaces =>
+		detectedFaces.map(face => {
+			const coords = face.region_info.bounding_box;
+			return {
+				id: face.id,
+				top: coords.top_row * 100,
+				left: coords.left_col * 100,
+				right: (1 - coords.right_col) * 100,
+				bottom: (1 - coords.bottom_row) * 100,
+			};
+		});
 
 	return (
 		<div className='App'>
 			<Particles className='particles' params={particlesOptions} />
-			<Navigation />
-			<Logo />
-			<Rank />
-			<ImageLinkForm onChange={handleChange} onClick={onClick} />
-			<Facerecognition url={inputUrl} boxes={boundingBoxes} />
+			<Navigation isSignIn={route === 'home'} onRouteChange={route => setRoute(route)} />
+
+			{route === 'home' ? (
+				<>
+					<Logo />
+					<Rank />
+					<ImageLinkForm
+						onChange={handleChange}
+						onClick={handleImageLinkFormClick}
+					/>
+					<Facerecognition url={inputUrl} boxes={boundingBoxes} />
+				</>
+			) : route === 'signIn' ? (
+				<SignIn onRouteChange={route => setRoute(route)} />
+			) : (
+				<Register />
+			)}
 		</div>
 	);
-}
+};
 
 export default App;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const Navigation = () => (
-    <nav className='flex justify-end'>
-        <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-    </nav>
-);
+const Navigation = ({ onRouteChange, isSignIn }) =>
+	isSignIn ? (
+		<nav className='flex justify-end'>
+			<a
+				href='#0'
+				onClick={() => onRouteChange('signIn')}
+				className='link dim black f3 underline pa3'
+			>
+				Sign Out
+			</a>
+		</nav>
+	) : (
+		<nav className='flex justify-end'>
+			<a
+				href='#0'
+				onClick={() => onRouteChange('signIn')}
+				className='link dim black f3 underline pa3'
+			>
+				Sign In
+			</a>
+			<a
+				href='#0'
+				onClick={() => onRouteChange('register')}
+				className='link dim black f3 underline pa3'
+			>
+				Register
+			</a>
+		</nav>
+	);
 
 export default Navigation;
