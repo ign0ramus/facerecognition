cfef4a7bf8697bd36bc7ccf37a26afd33add4aad
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-25 21:48:19 +0300

    add backend url

--- frontend/src/const/api.js
+++ frontend/src/const/api.js
@@ -1,4 +1,4 @@
-const API = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
+const API = 'https://sheltered-woodland-30586.herokuapp.com';
 
 export const SIGN_IN_API = `${API}/sign-in`;
 export const SIGN_UP_API = `${API}/sign-up`;
