874fd96f02a702530b619260473f27886b29f898
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-25 18:55:14 +0300

    add env clarifai api key

--- backend/api/clarifaiApi.js
+++ backend/api/clarifaiApi.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const clarifaiApp = new Clarifai.App({
-	apiKey: 'test',
+	apiKey: process.env.CLARIFAI_API_KEY,
 });
 
 const recognizeFace = async image => {
