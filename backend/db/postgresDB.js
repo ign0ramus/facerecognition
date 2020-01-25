6e7e039f42fc7fddf47ea636257013771f6cf0f3
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-25 21:16:17 +0300

    change env postgres to heroku

--- backend/db/postgresDB.js
+++ backend/db/postgresDB.js
@@ -3,10 +3,8 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.POSTGRES_URL || '127.0.0.1',
-        user: 'postgres',
-        password: '',
-        database: 'facerecognition'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
