009bb79a22557f5b3e0b117031ca630fb0322ad7
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-25 14:38:35 +0300

    refactor frontend

---
+++ frontend/src/components/Form/Input/Input.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Input = props => {
+    const { name, label, type, onChange, ...options } = props;
+	return (
+		<div className='mt3'>
+			<label className='db fw6 lh-copy f6' htmlFor={name}>
+				{label}
+			</label>
+			<input
+                className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                name={name}
+                type={type}
+                onChange={onChange}
+                {...options}
+			/>
+		</div>
+	);
+};
+
+export default Input;
