5673ca488aea0fc7275611a517655fa465106050
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-25 20:37:07 +0300

    for github pages

---
+++ frontend/build/precache-manifest.f938fcb95d9c51251d52eeb0e97dc20c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d1fea36a0892650ee5399f7b8c08cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f928d2dce1c33da700f",
+    "url": "/static/css/2.86cbe886.chunk.css"
+  },
+  {
+    "revision": "e4fb8175fe78a721d9bb",
+    "url": "/static/css/4.37e28ac2.chunk.css"
+  },
+  {
+    "revision": "2b37703ff86de4e48929",
+    "url": "/static/css/main.e7585ad0.chunk.css"
+  },
+  {
+    "revision": "8f928d2dce1c33da700f",
+    "url": "/static/js/2.92abab3a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.92abab3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b7f877538ad1c26b3a3",
+    "url": "/static/js/3.64762eb9.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/3.64762eb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4fb8175fe78a721d9bb",
+    "url": "/static/js/4.2aebf501.chunk.js"
+  },
+  {
+    "revision": "7f9867411a21819abe0b",
+    "url": "/static/js/5.4877b2e8.chunk.js"
+  },
+  {
+    "revision": "a3f49422fd7d794f5c1a",
+    "url": "/static/js/6.9757ccb3.chunk.js"
+  },
+  {
+    "revision": "2b37703ff86de4e48929",
+    "url": "/static/js/main.e716e1bb.chunk.js"
+  },
+  {
+    "revision": "3cdaa2d9b5b8f8654b45",
+    "url": "/static/js/runtime-main.04b7e717.js"
+  }
+]);
