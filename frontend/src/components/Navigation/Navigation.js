3eb2944385c9d06d384edd02bcf2375f867586b8
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-25 22:12:47 +0300

    fix navigation links

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import UserContext from '../../context/UserContext';
+import { SIGN_IN_URL, SIGN_UP_URL } from '../../const/urls';
 
 const LinkTachyonsClasses = 'link dim black f4 f3-ns underline pa3';
 const Navigation = () => {
@@ -8,16 +9,16 @@ const Navigation = () => {
 
 	return user ? (
 		<nav className='flex justify-end'>
-			<Link onClick={signOut} to='/sign-in' className={LinkTachyonsClasses}>
+			<Link onClick={signOut} to={SIGN_IN_URL} className={LinkTachyonsClasses}>
 				Sign out
 			</Link>
 		</nav>
 	) : (
 		<nav className='flex justify-center justify-end-ns'>
-			<Link to='/sign-in' className={LinkTachyonsClasses}>
+			<Link to={SIGN_IN_URL} className={LinkTachyonsClasses}>
 				Sign In
 			</Link>
-			<Link to='/sign-up' className={LinkTachyonsClasses}>
+			<Link to={SIGN_UP_URL} className={LinkTachyonsClasses}>
 				Sign Up
 			</Link>
 		</nav>
