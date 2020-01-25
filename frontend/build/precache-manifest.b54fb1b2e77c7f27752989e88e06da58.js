da9ff0a7418dd987fafbd093a582aabceba43e9b
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-25 19:55:35 +0300

    change homepage url

---
+++ frontend/build/precache-manifest.b54fb1b2e77c7f27752989e88e06da58.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa189e6a8faf73619b4eb61e57a2692",
+    "url": "/ign0ramus/facerecognition/index.html"
+  },
+  {
+    "revision": "8f928d2dce1c33da700f",
+    "url": "/ign0ramus/facerecognition/static/css/2.86cbe886.chunk.css"
+  },
+  {
+    "revision": "e4fb8175fe78a721d9bb",
+    "url": "/ign0ramus/facerecognition/static/css/4.37e28ac2.chunk.css"
+  },
+  {
+    "revision": "0518a4e19ca504baa6a1",
+    "url": "/ign0ramus/facerecognition/static/css/main.e7585ad0.chunk.css"
+  },
+  {
+    "revision": "8f928d2dce1c33da700f",
+    "url": "/ign0ramus/facerecognition/static/js/2.92abab3a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ign0ramus/facerecognition/static/js/2.92abab3a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b7f877538ad1c26b3a3",
+    "url": "/ign0ramus/facerecognition/static/js/3.64762eb9.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/ign0ramus/facerecognition/static/js/3.64762eb9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4fb8175fe78a721d9bb",
+    "url": "/ign0ramus/facerecognition/static/js/4.2aebf501.chunk.js"
+  },
+  {
+    "revision": "7f9867411a21819abe0b",
+    "url": "/ign0ramus/facerecognition/static/js/5.4877b2e8.chunk.js"
+  },
+  {
+    "revision": "a3f49422fd7d794f5c1a",
+    "url": "/ign0ramus/facerecognition/static/js/6.9757ccb3.chunk.js"
+  },
+  {
+    "revision": "0518a4e19ca504baa6a1",
+    "url": "/ign0ramus/facerecognition/static/js/main.c6a2be75.chunk.js"
+  },
+  {
+    "revision": "e486b4d22c365b8a38ce",
+    "url": "/ign0ramus/facerecognition/static/js/runtime-main.aa6164db.js"
+  }
+]);
