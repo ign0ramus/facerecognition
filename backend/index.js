3cb6001f2afcb27d17019a4bada01a72153fd896
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-27 12:14:48 +0300

    change sameSite in cookies

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(
 		secret: process.env.SESSION_SECRET || '!secret1',
 		cookie: {
 			maxAge: process.env.SESSION_LIFETIME || ONE_WEEK,
-			sameSite: false,
+			sameSite: 'strict',
 			httpOnly: true,
 		},
 	})
