7d83f19018e77d232e9eb35bdf1626088eabe570
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-27 13:15:42 +0300

    revert changes

--- frontend/src/const/urls.js
+++ frontend/src/const/urls.js
@@ -1,3 +1,3 @@
-export const HOME_URL = `${process.env.PUBLIC_URL}/`;
-export const SIGN_IN_URL = `${process.env.PUBLIC_URL}/sign-in`;
-export const SIGN_UP_URL = `${process.env.PUBLIC_URL}/sign-up`;
+export const HOME_URL = '/';
+export const SIGN_IN_URL = '/sign-in';
+export const SIGN_UP_URL = '/sign-up';
