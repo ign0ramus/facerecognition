58134b9bca7edd405cdf1f5d48fb7691bbdfccd0
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-02-23 11:22:30 +0300

    remove cors localhost

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5050;
 app.use(express.json());
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+		origin: process.env.FRONTEND_URL,
 	})
 );
 app.use(morgan('combined'));
